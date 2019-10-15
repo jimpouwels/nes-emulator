d94ebf1ebbea8c5f6e82095ee17697c8c32311b4
Jim Pouwels <jim.pouwels@philips.com>
2019-10-15 20:14:01 +0200

    improved UI

--- ui/src/main/java/nl/pouwels/nes/ui/MainScreen.java
+++ ui/src/main/java/nl/pouwels/nes/ui/MainScreen.java
@@ -5,36 +5,54 @@ import nl.pouwels.nes.ppu.Screen;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.geom.AffineTransform;
 import java.awt.image.BufferedImage;
 
 public class MainScreen extends JPanel implements Screen {
 
     private final BufferedImage canvas;
-    private BufferedImage img = new BufferedImage(500, 300, BufferedImage.TYPE_INT_RGB);
 
     public MainScreen() {
-        JFrame frame = new JFrame("NES Emulator");
-        frame.setPreferredSize(new Dimension(400, 300));
-        frame.add(this);
-        frame.pack();
-        frame.setVisible(true);
-        frame.setResizable(true);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        canvas = new BufferedImage(640, 480, BufferedImage.TYPE_INT_RGB);
-    }
-
-    public void paintComponent(Graphics g) {
-        super.paintComponent(g);
-        Graphics2D g2 = (Graphics2D) g;
-        g2.drawImage(canvas, null, null);
+        renderWindow();
+        renderStatus();
+        canvas = new BufferedImage(341, 261, BufferedImage.TYPE_INT_RGB);
     }
 
     @Override
     public void drawPixel(int x, int y, Pixel pixel) {
         int rgb = ((pixel.r & 0x0ff) << 16) | ((pixel.g & 0x0ff) << 8) | (pixel.b & 0x0ff);
         canvas.setRGB(x, y, rgb);
+
         if (x == 0 && y == 0) {
             repaint();
         }
+    }
+
+    private void renderStatus() {
+        Label statusTitle = new Label();
+        statusTitle.setText("STATUS");
+        statusTitle.setForeground(Color.WHITE);
+        statusTitle.setBounds(875, 0, 100, 40);
+        add(statusTitle);
+    }
+
+    private void renderWindow() {
+        JFrame frame = new JFrame("NES Emulator");
+        frame.setPreferredSize(new Dimension(1250, 675));
+        frame.add(this);
+        frame.pack();
+        frame.setVisible(true);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setBackground(Color.decode("0x022f8e"));
+        setLayout(null);
+    }
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        Graphics2D g2 = (Graphics2D) g;
+        AffineTransform imageSpaceTran = new AffineTransform();
+        imageSpaceTran.scale(2.5f, 2.5f);
+        g2.drawImage(canvas, imageSpaceTran, null);
     }
 }
