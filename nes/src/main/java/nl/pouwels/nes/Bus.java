d31c2b79188794585fd017fb7357e28c415a0cda
Jim Pouwels <jim.pouwels@philips.com>
2019-10-17 16:39:02 +0200

    Mask the address with lowest 3 bits to map it to the 3 bit range of the registers of ppu

--- nes/src/main/java/nl/pouwels/nes/Bus.java
+++ nes/src/main/java/nl/pouwels/nes/Bus.java
@@ -59,7 +59,7 @@ public class Bus {
         } else if (address_16 >= RAM_RANGE_START && address_16 <= RAM_RANGE_END) {
             return ram.cpuReadByte(address_16);
         } else if (address_16 >= PPU_RANGE_START && address_16 <= PPU_RANGE_END) {
-            return ppu.cpuRead(address_16);
+            return ppu.cpuRead(address_16 & 0x0007);
         }
         return 0x00;
     }
